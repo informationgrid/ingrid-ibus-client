05980d6ff553823c3ce2488bb3590d713837750b
hz <hz@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-03-31 18:14:44 +0000

    add testZombieThreafs

--- src/test/de/ingrid/BustClientTest.java
+++ src/test/de/ingrid/BustClientTest.java
@@ -25,46 +25,42 @@ public class BustClientTest extends TestCase {
 
     private static final boolean ENABLED = false;
 
-    // private static final String BUS_URL="wetag:///group:ibus";
-    //    
-    // private static final String
-    // FACADE_JXTA_CONFIG="/de/ingrid/test.facade.jxta.conf.xml";
+    /**
+     * @throws Throwable
+     */
+    public void testZombieThreafs() throws Throwable {
+        if (!ENABLED) {
+            System.out.println("skipping testZombieThreads");
+            return;
+        }
+        BusClient client = BusClient.instance();
+        String busUrl = "wetag:///IBUS-SERVER-INDERECKE:IBus-inderecke";
+        String jxtaConf = "/de/ingrid/localtest.jxta.conf.xml";
+        client.setBusUrl(busUrl);
+        client.setJxtaConfigurationPath(jxtaConf);
 
-    // ProxyService proxyService;
-    //    
-    // PeerService peerService;
-    // Bus bus;
-    //    
-    // protected void setUp() throws Exception {
-    // String jxtaHome = "target/busserver_.jxta";
-    // deleteDirectoryRec(jxtaHome);
-    // this.peerService = new PeerService();
-    // setPeerServiceVariables(this.peerService, jxtaHome, 9701, 9700);
-    // this.peerService.boot();
-    // // this.peerService.subscribeGroup(BUS_URL);
-    //        
-    // this.proxyService = new ProxyService();
-    // this.proxyService.setCommunication(this.peerService);
-    // this.proxyService.startup();
-    //        
-    // IPlugProxyFactory proxyFactory = new IPlugProxyFactoryImpl(peerService);
-    // bus = new Bus(proxyFactory);
-    // Registry registry = bus.getIPlugRegistry();
-    // registry.setCommunication(peerService);
-    // }
+        printThreadStatistic();
+        Bus bus = client.getBus();
+        assertNotNull(bus);
 
-    // protected void tearDown() throws Exception {
-    // this.proxyService.shutdown();
-    // this.peerService.shutdown();
-    // }
+        client.shutdown();
+        Thread.sleep(1000);
+        printThreadStatistic();
+    }
 
-    // public void testGetBus() throws IOException {
-    // Facade facade=Facade.instance();
-    // facade.setBusUrl(BUS_URL);
-    // facade.setJxtaConfigurationPath(FACADE_JXTA_CONFIG);
-    // Bus bus=facade.getBus();
-    // assertNotNull(bus.getAllIPlugs());
-    // }
+    private void printThreadStatistic() {
+        System.out.println("**********************************");
+        System.out.println(Thread.currentThread().getThreadGroup());
+        System.out.println("threads:" + Thread.currentThread().getThreadGroup().activeCount());
+        System.out.println("groups:" + Thread.currentThread().getThreadGroup().activeGroupCount());
+        ThreadGroup[] threadGroups = new ThreadGroup[Thread.currentThread().getThreadGroup().activeGroupCount()];
+        Thread.currentThread().getThreadGroup().enumerate(threadGroups);
+        System.out.println("------------thread groups-----------------");
+        for (int i = 0; i < threadGroups.length; i++) {
+            System.out.println(threadGroups[i].getName() + " - parent :" + threadGroups[i].getParent().getName());
+        }
+        System.out.println("**********************************");
+    }
 
     /**
      * @throws IOException
@@ -123,6 +119,76 @@ public class BustClientTest extends TestCase {
             }
         }
     }
+
+    // private static final String BUS_URL="wetag:///group:ibus";
+    //    
+    // private static final String
+    // FACADE_JXTA_CONFIG="/de/ingrid/test.facade.jxta.conf.xml";
+
+    // ProxyService proxyService;
+    //    
+    // PeerService peerService;
+    // Bus bus;
+    //    
+    // protected void setUp() throws Exception {
+    // String jxtaHome = "target/busserver_.jxta";
+    // deleteDirectoryRec(jxtaHome);
+    // this.peerService = new PeerService();
+    // setPeerServiceVariables(this.peerService, jxtaHome, 9701, 9700);
+    // this.peerService.boot();
+    // // this.peerService.subscribeGroup(BUS_URL);
+    //        
+    // this.proxyService = new ProxyService();
+    // this.proxyService.setCommunication(this.peerService);
+    // this.proxyService.startup();
+    //        
+    // IPlugProxyFactory proxyFactory = new IPlugProxyFactoryImpl(peerService);
+    // bus = new Bus(proxyFactory);
+    // Registry registry = bus.getIPlugRegistry();
+    // registry.setCommunication(peerService);
+    // }
+
+    // protected void tearDown() throws Exception {
+    // this.proxyService.shutdown();
+    // this.peerService.shutdown();
+    // }
+
+    // public void testGetBus() throws IOException {
+    // Facade facade=Facade.instance();
+    // facade.setBusUrl(BUS_URL);
+    // facade.setJxtaConfigurationPath(FACADE_JXTA_CONFIG);
+    // Bus bus=facade.getBus();
+    // assertNotNull(bus.getAllIPlugs());
+    // }
+
+    // public void testGetTorwaldBus() throws IOException {
+    // BusClient client = BusClient.instance();
+    // String jxtaConf = "/jxta.conf.xml";
+    // client.setJxtaConfigurationPath(jxtaConf);
+    //
+    // Bus bus = client.getBus();
+    // assertNotNull(bus);
+    // assertNull(bus.getIPlug("nixIplug"));
+    //
+    // client.setJxtaConfigurationPath("/irgendwas/falsches.ccp");
+    // try {
+    // client.reconnect();
+    // fail("jxta conf not exists");
+    // } catch (RuntimeException e) {
+    // //
+    // }
+    //
+    // client.setJxtaConfigurationPath(jxtaConf);
+    // bus = client.reconnect();
+    // assertNotNull(bus);
+    // assertNull(bus.getIPlug("nixIplug"));
+    //
+    // PeerService peerService = (PeerService) client.getCommunication();
+    // String jxtaHome = peerService.getJxtaHome();
+    // client.shutdown();
+    // deleteDirectoryRec(jxtaHome);
+    //
+    // }
 
     // private static void setPeerServiceVariables(AbstractJxtaConfiguration p,
     // String jh, int tcpPort, int httpPort) {
