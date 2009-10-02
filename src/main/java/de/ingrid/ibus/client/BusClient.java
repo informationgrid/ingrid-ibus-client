b42553c0f96be33f53818c89b64423fbd24b1e05
me.101tec <me.101tec@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2009-10-02 08:53:19 +0000

    improve api

--- src/main/java/de/ingrid/ibus/client/BusClient.java
+++ src/main/java/de/ingrid/ibus/client/BusClient.java
@@ -7,45 +7,77 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
 
 import net.weta.components.communication.ICommunication;
+import net.weta.components.communication.messaging.IMessageHandler;
+import net.weta.components.communication.messaging.IMessageQueue;
 import net.weta.components.communication.reflect.ReflectInvocationHandler;
+import net.weta.components.communication.reflect.ReflectMessageHandler;
 import net.weta.components.communication.tcp.StartCommunication;
 import net.weta.components.communication.tcp.TcpCommunication;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import de.ingrid.utils.IBus;
+import de.ingrid.utils.IPlug;
 
 public class BusClient {
 
-    private IBus _nonCacheableIBus;
+	private IBus _nonCacheableIBus;
 
     private IBus _cacheableIBus;
 
     private ICommunication _communication;
 
+	private File _communicationXml;
+
+	private IPlug _iPlug;
+
+	private static final Log LOG = LogFactory.getLog(BusClient.class);
+
+	@Deprecated
     BusClient() throws Exception {
         this(BusClient.class.getResourceAsStream("/communication.xml"));
     }
 
-    BusClient(File communicationXml) throws Exception {
-        this(new FileInputStream(communicationXml));
+    BusClient(final File communicationXml) throws Exception {
+		_communicationXml = communicationXml;
+		start();
     }
 
-    BusClient(InputStream inputStream) throws Exception {
+	BusClient(final File communicationXml, final IPlug iplug) throws Exception {
+		_communicationXml = communicationXml;
+		_iPlug = iplug;
+		start();
+	}
+
+	@Deprecated
+    BusClient(final InputStream inputStream) throws Exception {
         _communication = StartCommunication.create(inputStream);
         _communication.startup();
         createIBusProxies(_communication);
     }
 
-    BusClient(ICommunication communication) throws Exception {
+	@Deprecated
+    BusClient(final ICommunication communication) throws Exception {
         _communication = communication;
         createIBusProxies(_communication);
     }
-    
-    private void createIBusProxies(ICommunication communication) throws Exception {
-        String busUrl = (String) ((TcpCommunication) communication).getServerNames().get(0);
-        InvocationHandler nonCacheableHandler = new ReflectInvocationHandler(communication, busUrl);
-        InvocationHandler cacheableInvocationHandler = new CacheableInvocationHandler(nonCacheableHandler);
-        _nonCacheableIBus = (IBus) Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(), new Class[] { IBus.class }, nonCacheableHandler);
-        _cacheableIBus = (IBus) Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(), new Class[] { IBus.class }, cacheableInvocationHandler);
-    }
+
+	public String getPeerName() {
+		return _communication.getPeerName();
+	}
+
+	public void setIPlug(final IPlug iPlug) throws Exception {
+		if (_iPlug != null) {
+			LOG.warn("iPlug is already set: " + _iPlug.getClass().getName());
+		} else {
+			_iPlug = iPlug;
+		}
+	}
+
+	public IPlug getIPlug() {
+		return _iPlug;
+	}
 
     public IBus getNonCacheableIBus() {
         return _nonCacheableIBus;
@@ -58,7 +90,88 @@ public class BusClient {
     public void close() throws Exception {
         _cacheableIBus.close();
         _nonCacheableIBus.close();
-        _communication.shutdown();
+		_communication.shutdown();
     }
 
+	public final String getUrl() {
+		if (_communication != null) {
+			return (String) ((TcpCommunication) _communication).getServerNames().get(0);
+		}
+		return null;
+	}
+
+	public boolean isConnected() {
+		if (_communication != null) {
+			return _communication.isConnected(getUrl());
+		}
+		return false;
+	}
+
+    public void start() throws Exception {
+		if (!isConnected() && _communicationXml.exists()) {
+			// connect
+			LOG.info("create communication");
+			_communication = StartCommunication.create(new FileInputStream(_communicationXml));
+			LOG.info("start communication");
+			_communication.startup();
+			// sleep until connected
+			for (int i = 0; i < 10; i++) {
+				if (isConnected()) {
+					break;
+				}
+				Thread.sleep(500);
+			}
+			if (!isConnected()) {
+				throw new Exception("start communication failed");
+			}
+			// create iBusses
+			createIBusProxies(_communication);
+			// set plug
+			setCommunicationPlug(_iPlug);
+		}
+	}
+
+	public void shutdown() throws Exception {
+		if (_communication != null && isConnected()) {
+			LOG.info("shutdown communication");
+			_communication.shutdown();
+			// sleep until connected
+			for (int i = 0; i < 10; i++) {
+				if (!isConnected()) {
+					break;
+				}
+				Thread.sleep(500);
+			}
+			if (isConnected()) {
+				throw new Exception("shutdown communication failed");
+			}
+		}
+	}
+
+	public void restart() throws Exception {
+		LOG.info("restart communication");
+
+		shutdown();
+
+		start();
+	}
+
+	private void createIBusProxies(final ICommunication communication) throws Exception {
+		final String busUrl = (String) ((TcpCommunication) communication).getServerNames().get(0);
+		final InvocationHandler nonCacheableHandler = new ReflectInvocationHandler(communication, busUrl);
+		final InvocationHandler cacheableInvocationHandler = new CacheableInvocationHandler(nonCacheableHandler);
+		_nonCacheableIBus = (IBus) Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(), new Class[] { IBus.class }, nonCacheableHandler);
+		_cacheableIBus = (IBus) Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(), new Class[] { IBus.class }, cacheableInvocationHandler);
+	}
+
+	private void setCommunicationPlug(final IPlug plug) {
+		if (plug != null && _communication != null) {
+			final IMessageQueue messageQueue = _communication.getMessageQueue();
+			final IMessageHandler messageHandler = new ReflectMessageHandler();
+			LOG.info("add iplug [" + plug.getClass().getSimpleName() + "] to message handler");
+			((ReflectMessageHandler) messageHandler).addObjectToCall(IPlug.class, plug);
+			LOG.info("add message handler to message queue");
+			messageQueue.addMessageHandler(ReflectMessageHandler.MESSAGE_TYPE, messageHandler);
+		}
+	}
 }
--- src/main/java/de/ingrid/ibus/client/BusClientFactory.java
+++ src/main/java/de/ingrid/ibus/client/BusClientFactory.java
@@ -1,31 +1,48 @@
 package de.ingrid.ibus.client;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.InputStream;
 
 import net.weta.components.communication.ICommunication;
+import de.ingrid.utils.IPlug;
 
 public class BusClientFactory {
 
     private static BusClient _busClient = null;
 
+	@Deprecated
     public static BusClient createBusClient() throws Exception {
         return createBusClient(BusClientFactory.class.getResourceAsStream("/communication.xml"));
     }
 
-    public static BusClient createBusClient(File file) throws Exception {
-        return createBusClient(new FileInputStream(file));
+    public static BusClient createBusClient(final File file) throws Exception {
+		if (_busClient == null) {
+			_busClient = new BusClient(file);
+		}
+		return _busClient;
     }
 
-    public static BusClient createBusClient(InputStream inputStream) throws Exception {
+	public static BusClient createBusClient(final File file, final IPlug iplug) throws Exception {
+		if (_busClient == null) {
+			_busClient = new BusClient(file, iplug);
+		}
+		return _busClient;
+	}
+
+	public static BusClient getBusClient() {
+		return _busClient;
+	}
+
+	@Deprecated
+    public static BusClient createBusClient(final InputStream inputStream) throws Exception {
         if (_busClient == null) {
             _busClient = new BusClient(inputStream);
         }
         return _busClient;
     }
-    
-    public static BusClient createBusClient(ICommunication communication) throws Exception {
+
+	@Deprecated
+    public static BusClient createBusClient(final ICommunication communication) throws Exception {
         if (_busClient == null) {
             _busClient = new BusClient(communication);
         }
