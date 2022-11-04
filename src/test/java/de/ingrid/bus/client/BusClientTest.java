3b145a9556da0f114e107e870ba61e98c7f205ec
Jan Bücher <jan.buecher@wemove.com>
2022-11-04 18:29:38 +0100

    Update to Mockito 3.12.4

--- src/test/java/de/ingrid/bus/client/BusClientTest.java
+++ src/test/java/de/ingrid/bus/client/BusClientTest.java
@@ -63,7 +63,7 @@ public class BusClientTest {
 
     @BeforeEach
     public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
+        MockitoAnnotations.openMocks(this);
 
         // new
         _server = StartCommunication.create(BusClientTest.class.getResourceAsStream("/communication-server.xml"));
